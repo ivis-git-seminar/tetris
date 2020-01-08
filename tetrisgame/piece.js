60340ea9587098717aa7bc2d41ae3a4a41e8964b
dyjung123 <jeongdongryun@gmail.com>
2020-01-08 16:22:13 +0900

    Feat: add all the features of the piece

--- tetrisgame/piece.js
+++ tetrisgame/piece.js
@@ -4,6 +4,8 @@ class Piece {
   color;
   shape;
   ctx;
+  typeId;
+  hardDropped;
 
   constructor(ctx) {
     this.ctx = ctx;
@@ -11,25 +13,18 @@ class Piece {
   }
 
   spawn() {
-    this.color = 'blue';
-    this.shape = [
-      [2, 0, 0],
-      [2, 2, 2],
-      [0, 0, 0]
-    ];
-
-    // Starting position.
-    this.x = 3;
+    this.typeId = this.randomizeTetrominoType(COLORS.length - 1);
+    this.shape = SHAPES[this.typeId];
+    this.color = COLORS[this.typeId];
+    this.x = 0;
     this.y = 0;
+    this.hardDropped = false;
   }
 
   draw() {
     this.ctx.fillStyle = this.color;
     this.shape.forEach((row, y) => {
       row.forEach((value, x) => {
-        // this.x, this.y는 shape의 상단 왼쪽 좌표이다
-        // shape 안에 있는 블록 좌표에 x, y를 더한다.
-        // 보드에서 블록의 좌표는 this.x + x가 된다.
         if (value > 0) {
           this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
         }
@@ -38,7 +33,22 @@ class Piece {
   }
 
   move(p) {
-    this.x = p.x;
-    this.y = p.y;
+    if(!this.hardDropped){
+      this.x = p.x;
+      this.y = p.y;
+    }
+    this.shape = p.shape;
+  }
+
+  hardDrop(){
+    this.hardDropped = true;
+  }
+
+  setStartingPosition() {
+    this.x = this.typeId === 4 ? 4 : 3;
+  }
+
+  randomizeTetrominoType(noOfTypes) {
+    return Math.floor(Math.random() * noOfTypes + 1);
   }
 }
