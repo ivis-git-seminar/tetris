3575e6496ea77dff1d47ded775776c420ae77333
dyjung123 <jeongdongryun@gmail.com>
2020-01-08 14:50:38 +0900

    Feat: added function to move pieces and process key events

--- tetrisgame/main.js
+++ tetrisgame/main.js
@@ -8,9 +8,38 @@ ctx.canvas.height = ROWS * BLOCK_SIZE;
 // 블록의 크기를 변경한다.
 ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
 
+moves = {
+  [KEY.LEFT]: p => ({ ...p, x: p.x - 1 }),
+  [KEY.RIGHT]: p => ({ ...p, x: p.x + 1 }),
+  [KEY.DOWN]: p => ({ ...p, y: p.y + 1 }),
+  [KEY.SPACE]: p => ({ ...p, y: p.y + 1 }),
+  [KEY.UP]: p => board.rotate(p)
+};
+
 let board = new Board();
+
+document.addEventListener('keydown', event => {
+  if (moves[event.keyCode]) {
+    // 이벤트 버블링을 막는다.
+    event.preventDefault();
+
+    // 조각의 새 상태를 얻는다.
+    let p = moves[event.keyCode](board.piece);
+
+    if (board.valid(p)) {
+      // 이동이 가능한 상태라면 조각을 이동한다.
+      board.piece.move(p);
+
+      // 그리기 전에 이전 좌표를 지운다.
+      ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+      board.piece.draw();
+    }
+  }
+});
 
 function play() {
   board.reset();
   console.table(board.grid);
 }
+
