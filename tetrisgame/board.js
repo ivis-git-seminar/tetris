bb464f6130c93fdc8c20dc9cbb944d9bbec987db
dyjung123 <jeongdongryun@gmail.com>
2020-01-08 14:33:32 +0900

    Feat: added the function to shape a tetris piece

--- tetrisgame/board.js
+++ tetrisgame/board.js
@@ -1,5 +1,22 @@
 class Board {
+  ctx;
+  ctxNext;
   grid;
+
+  constructor(ctx, ctxNext) {
+    this.ctx = ctx;
+    this.ctxNext = ctxNext;
+    this.init();
+  }
+
+  init() {
+    // Calculate size of canvas from constants.
+    this.ctx.canvas.width = COLS * BLOCK_SIZE;
+    this.ctx.canvas.height = ROWS * BLOCK_SIZE;
+
+    // Scale so we don't need to give size on every draw.
+    this.ctx.scale(BLOCK_SIZE, BLOCK_SIZE);
+  }
 
   // 새 게임이 시작되면 보드를 초기화한다.
   reset() {
